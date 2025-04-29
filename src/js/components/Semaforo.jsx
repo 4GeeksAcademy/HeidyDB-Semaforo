00168cc9ccf52a6c822b36db892b49f87f3ecb35
HeidyDB <heidydiazbarrios@gmail.com>
2025-04-29 14:02:37 +0000

    esta mejorado lo visual del semaforo

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -5,46 +5,37 @@ import { useState } from "react";
 const Semaforo = () => {
 
 
-    const [color, setColor] = useState("red");
-	
-	
-    return (
-        <div>
+	const [color, setColor] = useState("red");
 
-<div className="container-fluid p-0">
-			<div
-				className="btn-group position-absolute top-0 start-50 translate-middle-x mt-2"
-				role="group"
-				>
-		    </div>
-			
 
-		<div className="soporteSemaforo bg-black"></div>
-		<div className="semaforo bg-black rounded d-flex flex-column">
-				<div
-					className={`${
-						color=== "red" ? "luzEncendida" : ""
-					} luces bg-danger m-auto`}
-					onClick={() => setColor("red")}></div>
-				<div
-					className={`${
-						color=== "yellow" ? "luzEncendida" : ""
-					} luces bg-warning m-auto`}
-					onClick={() => setColor("yellow")}></div>
-				<div
-					className={`${
-						color=== "green" ? "luzEncendida" : ""
-					} luces bg-success m-auto`}
-					onClick={() => setColor("green")}></div>
-			
+	return (
+		<div>
+
+			<div className="container-fluid p-0">
+				<div className="btn-group position-absolute top-0 start-50 translate-middle-x mt-2"
+					role="group">
+				</div>
+
+				<div className="paloSemaforo bg-black"></div>
+				<div className="semaforo bg-black rounded d-flex flex-column">
+					<div
+						className={`${color === "red" ? "luzEncendidaRoja" : ""
+							} luces bg-danger m-auto`}
+						onClick={() => setColor("red")}></div>
+					<div
+						className={`${color === "yellow" ? "luzEncendidaAmarilla" : ""
+							} luces bg-warning m-auto`}
+						onClick={() => setColor("yellow")}></div>
+					<div
+						className={`${color === "green" ? "luzEncendidaVerde" : ""
+							} luces bg-success m-auto`}
+						onClick={() => setColor("green")}></div>
+
+				</div>
 			</div>
-		</div> 
-                
-        </div>
-		
-       
-		
-    );
+
+		</div>
+	);
 };
 
 export default Semaforo;
