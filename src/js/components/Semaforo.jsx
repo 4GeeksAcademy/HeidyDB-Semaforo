a2e38a9236153d7144c9317366882e7c3c0068bc
HeidyDB <heidydiazbarrios@gmail.com>
2025-04-29 18:10:44 +0000

    semaforo done

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -7,6 +7,20 @@ const Semaforo = () => {
 
 	const [color, setColor] = useState("red");
 
+	const cambiarColorBoton = () => { // cambiar la luz del semaforo c el boton 
+
+		if (color === "red") {
+			setColor("yellow")
+		} else if (color === "yellow") {
+			setColor("green")
+		} else {
+			setColor("red")
+		}
+	}
+
+	const ponerColorLila = () => { // cambiar la luz del semaforo c el boton 
+		setColor("lila")
+	}
 
 	return (
 		<div>
@@ -18,23 +32,31 @@ const Semaforo = () => {
 
 				<div className="paloSemaforo bg-black"></div>
 				<div className="semaforo bg-black rounded d-flex flex-column">
-					<div
-						className={`${color === "red" ? "luzEncendidaRoja" : ""
-							} luces bg-danger m-auto`}
+					<div className={`${color === "red" ? "luzEncendidaRoja" : ""
+						} luces bg-danger m-auto`}
 						onClick={() => setColor("red")}></div>
-					<div
-						className={`${color === "yellow" ? "luzEncendidaAmarilla" : ""
-							} luces bg-warning m-auto`}
+
+					<div className={`${color === "yellow" ? "luzEncendidaAmarilla" : ""
+						} luces bg-warning m-auto`}
 						onClick={() => setColor("yellow")}></div>
-					<div
-						className={`${color === "green" ? "luzEncendidaVerde" : ""
-							} luces bg-success m-auto`}
+
+					<div className={`${color === "green" ? "luzEncendidaVerde " : ""
+						} luces bg-success m-auto`}
 						onClick={() => setColor("green")}></div>
 
+
+					<div className={`${color === "lila" ? "luzEncendidaLila luces bg-violet m-auto" : ""
+						} `}
+						onClick={() => setColor("lila")}></div>
 				</div>
 			</div>
+			<button type="button" className="btn btn-dark"
+				onClick={cambiarColorBoton}>Cambiar luces </button>
 
+			<button type="button" className="btn btn-dark"  
+				onClick={ponerColorLila}>Agregar lila </button>
 		</div>
+
 	);
 };
 
